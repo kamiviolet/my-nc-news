209feb9ed3924246be2b313af50cbeeef8f791cc
kamiviolet <kamiviolet@gmail.com>
2023-05-09 15:22:04 +0100

    done basic error-handling, put down routers

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const topics = require('./routers/topics.router');
-
+const {
+    getInstructions
+} = require('./controllers/main.controller')
+const { getTopics } = require('./controllers/topics.controller')
 const app = express();
 
-app.use('/api/topics', topics);
+app.use(express.json());
+
+app.route('/api/topics')
+    .get(getTopics)
 
 //Error-handler
+app.use('/*', (req, res, next) => {
+    const err = {status: 404, message: 'Not found.'};
+    next(err);
+})
+
 app.use((err, req, res, next) => {
-    if (err) {
-        console.error(err.message);
+    if (err.code === '22PO2') {
+        res.status(400).send({message: 'Bad request.'});
+    } else if (err.status && err.message) {
+        res.status(err.status).send({message: err.message});
     }
 })
+
 
 module.exports = app;
