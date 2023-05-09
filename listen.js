78a5d4ff9381f6631457cfe7d4c0f5eec2413548
kamiviolet <kamiviolet@gmail.com>
2023-05-09 13:35:35 +0100

    add logics to listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,7 @@
 const app = require('./app');
 const port = 8888;
 
-app.listen(port);
+app.listen(port, (err) => {
+    if (err) { console.err(err.message) }
+    else { console.log(`The server is running on ${port}.`) }
+});
