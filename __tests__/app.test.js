2ad20b6445b5b3b2e946ec6937d779f8c2a6bb20
kamiviolet <kamiviolet@gmail.com>
2023-05-10 10:16:07 +0100

    polish the description in test suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,8 +12,20 @@ afterAll(() => {
     if (connection) connection.end();
 })
 
-describe('GET /api/topics', () => {
-    it('status 200, responds with an array of topic objects,  each of which should have the following properties: slug, description.', () => {
+describe('/api/not-a-route', () => {
+    it('GET - status 404 - invalid endpoints', () => {
+        return request(app)
+            .get('/api/not-a-route')
+            .expect(404)
+            .then(({body}) => {
+                const {message} = body;
+                expect(message).toBe('Not found.')
+            })
+    })
+})
+
+describe('/api/topics', () => {
+    it('GET - status 200 - an array of topic objects,  each of which should have the following properties: slug, description.', () => {
         return request(app)
             .get('/api/topics')
             .expect(200)
@@ -32,21 +44,9 @@ describe('GET /api/topics', () => {
     })
 })
 
-describe('GET /api/not-a-route', () => {
-    it('status 404 if the endpoint is not valid', () => {
-        return request(app)
-            .get('/api/not-a-route')
-            .expect(404)
-            .then(({body}) => {
-                const {message} = body;
-                expect(message).toBe('Not found.')
-            })
-    })
-})
 
-
-describe('GET /api/articles/:article_id', () => {
-    it('status 200, respond with the correct data by article_id', () => {
+describe('/api/articles/:article_id', () => {
+    it('GET - status 200 - respond with the correct data by article_id', () => {
         return request(app)
             .get('/api/articles/2')
             .expect(200)
@@ -63,7 +63,7 @@ describe('GET /api/articles/:article_id', () => {
             })
     })
 
-    it('status 404, invalid numeric article_id will respond with bad request', () => {
+    it('GET - status 404 - invalid numeric article_id will respond with not found.', () => {
         return request(app)
             .get('/api/articles/99999')
             .expect(404)
@@ -74,7 +74,7 @@ describe('GET /api/articles/:article_id', () => {
     })
 
 
-    it('status 400, invalid non-numeric article_id will respond with bad request', () => {
+    it('GET - status 400 - invalid non-numeric article_id will respond with bad request', () => {
         return request(app)
             .get('/api/articles/non-sense')
             .expect(400)
