84bbe97d8e575659c8f008960d7dae51aae1fb6a
kamiviolet <kamiviolet@gmail.com>
2023-05-11 12:38:55 +0100

    erase unnecessary logics in controller and model

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -15,22 +15,14 @@ const getCommentsByArticleId = (req, res, next) => {
 const postNewCommentByArticleId = (req, res, next) => {
     const newComment = req.body;
     const {article_id} = req.params;
-    if (Object.keys(newComment).length === 0) {
-        const err = {status: 400, message: 'It seems you forget to send the request...'}
-        next(err);
-    } else if (!Object.hasOwn(newComment, 'username') || !Object.hasOwn(newComment, 'body')) {
-        const err = {status: 400, message: 'Comment not in correct format.'}
-        next(err);
-    } else if (Number.isNaN(+article_id) !== false) {
-        const err = {status: 400, message: 'Invalid article_id.'}
-        next(err);
-    } else {
-        return createNewCommentByArticleId(article_id, newComment)
-            .then((comment) => {
-                return res.status(201).send({comment})
-            })
-            .catch(err => next(err))
-    }
+
+    return createNewCommentByArticleId(article_id, newComment)
+        .then((comment) => {
+            res.status(201).send({comment: comment[0]})
+        })
+        .catch(err => {
+            return next(err)
+        })
 }
 
 module.exports = { getCommentsByArticleId, postNewCommentByArticleId }
--- models/comments.model.js
+++ models/comments.model.js
@@ -20,26 +20,13 @@ exports.fetchCommentsByArticleId = (id) => {
 }
 
 exports.createNewCommentByArticleId = (id, comment) => {
-    return validateExistingId(id)
-        .then((msg) => {
-            if (msg !== undefined) {
-                return Promise.reject({status: 400, message: 'Apology. Non-existing article_id.'})
-            }
-        })
-        .then(() => validateExistingUser(comment.username))
-        .then((msg) => {
-            if (msg !== undefined) {
-                return Promise.reject({status: 400, message: 'Apology. Non-existing username, please sign up to leave your comment.'})
-            }
-        })
-        .then(() => {
-            return db.query(`
+    return db
+        .query(`
                 INSERT INTO comments
                 (article_id, body, author)
                 VALUES
                 ($1, $3, $2)
                 RETURNING *;
             `, [id, comment.username, comment.body])
-        })
-        .then(({rows}) => rows[0])
+        .then(({rows}) => rows)
 }
