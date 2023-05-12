02076d49fc6a3d50adf0ba4f3e2a19f50e0ab3a2
kamiviolet <kamiviolet@gmail.com>
2023-05-12 14:08:50 +0100

    add new method and endpoint GET /api/users, related test and update to endpoints.json

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { getInstructions } = require('./controllers/main.controller')
 const { getArticleById, getAllArticles, patchVotesByArticleId } = require('./controllers/articles.controller')
 const { getCommentsByArticleId, postNewCommentByArticleId, deleteCommentByCommentId } = require('./controllers/comments.controller')
 const { handleDatabaseError, handleCustomError, handleRestError, handleInvalidEndpoint } = require('./controllers/errors.handler')
+const { getAllUsers } = require('./controllers/users.controller')
+
 const app = express();
 
 app.use(express.json())
@@ -27,6 +29,10 @@ app.route('/api/articles/:article_id/comments')
 
 app.route('/api/comments/:comment_id')
     .delete(deleteCommentByCommentId)
+
+app.route('/api/users')
+    .get(getAllUsers)
+
 
 app.use(handleInvalidEndpoint)
     
